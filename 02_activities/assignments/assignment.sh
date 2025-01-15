768acd3c7535c0c90120fd4bbef56ce8c8fdd14d
capa-detated <139367571+capa-detated@users.noreply.github.com>
2025-01-15 14:59:22 -0500

    modified file to perform all tasks necessary for the file processing

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -11,6 +11,7 @@ set -x
 
 mkdir analysis output
 touch README.md
+echo "# Project Name: DSI Consulting Inc." > README.md
 touch analysis/main.py
 
 # download client data
@@ -18,24 +19,33 @@ curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_ac
 unzip rawdata.zip
 
 ###########################################
+
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data 
+cd data
+mkdir raw processed
+cd ..
 # 2. Move the ./rawdata directory to ./data/raw
-
+mv ./rawdata/* ./data/raw
 # 3. List the contents of the ./data/raw directory
-
+ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-
+cd ./data/processed
+mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cd ../../
+cp ./data/raw/server*.log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-
+cp ./data/raw/user*.log ./data/processed/user_logs
+cp ./data/raw/event*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rm -f ./data/raw/*ipaddr*
+rm -f ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+touch ./data/inventory.txt
+ls -R ./data/processed > ./data/inventory.txt
 
 
 ###########################################
